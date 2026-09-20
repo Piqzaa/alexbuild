8a3b0205a31b6c4fdf655f0d2171c7f422e0452c
Alex B <alex.berrel@gmail.com>
2026-09-21 00:35:12 +0200

    feat: repositionne les offres et ajoute l'automatisation

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ import { initAnimations } from './animations.js';
 import { initAuditTool } from './audit-tool.js';
 import { initExperience } from './experience.js';
 import { initAmbientParticles } from './particles.js?v=20260920';
-import { initContactForm } from './contact-form.js';
+import { initContactForm } from './contact-form.js?v=20260921a';
 import { initLaunchSphere } from './launch-sphere.js?v=20260920d';
 import { initPowerWatcher } from './power.js';
 
